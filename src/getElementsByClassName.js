c1fb7099de4467f715ebfa7f851e9867115f4fa3
Donald Steinert <steinert.donald@gmail.com>
2015-07-23 09:10:48 -0400

    compose function to iterate through class lists of all document elements and its descendents

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,27 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-){
-  // your code here
+var getElementsByClassName = function(className){
+  var elementNodeReference = arguments[1] || document;
+  var nodeList;
+  if (elementNodeReference === document) {
+    nodeList = elementNodeReference.body.childNodes;
+  } else {
+    nodeList = arguments[1].childNodes; 
+  }
+  for (var i = 0; i < nodeList.length; i++) {
+      var classes = nodeList[i].classList;
+      if (classes !== undefined && classes.length > 0) {
+        console.log(classes);
+        for (var j = 0; j < classes.length; j++) {
+          console.log(classes[j]);
+          if (classes[j] == className) {
+            console.log(nodeList[i]);
+          }
+        }
+      }
+    if (nodeList[i].childNodes) {
+      getElementsByClassName(className, nodeList[i]);
+    }
+  }
 };
