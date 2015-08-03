18631ecbc2e88bce8329f91da0466b91bcb36912
Donald Steinert <steinert.donald@gmail.com>
2015-08-03 10:22:25 -0400

    refactor and simplify get elements by class name

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -5,47 +5,32 @@
 
 // But instead we're going to implement it from scratch:
 var getElementsByClassName = function(className) {
-  var elementsWithClass;
+  var elementsWithClass = [];
   var classNames = className.split(' ');
-  
-  var elementNodeReference = arguments[1] || document;
-  var nodeList = elementNodeReference.childNodes;
-  var nodeListLength = nodeList.length;
-  if (nodeListLength) {
-    for (var i = 0; i < nodeList.length; i++) {
-      
-      // determines if node has a class list
-      if (nodeList[i].classList) {
-        var nodeClasses = nodeList[i].classList;
-        
-        // determines if node has class
-        for (var j = 0; j < nodeClasses.length; j++) {
-          for (var k = 0; k < classNames.length; k++) {
-            if (nodeClasses[j] === classNames[k]) {
-              if (elementsWithClass === undefined) {
-                elementsWithClass = [];
-              }
-              elementsWithClass.push(nodeList[i]);
-            }
-          }
-        }
-      }
-      
-      // recursive call function if node has children elements
-      if (nodeList[i].childNodes.length >= 1) {
-        var childElementsWithClass = getElementsByClassName(className, nodeList[i]);
-        if (childElementsWithClass) {
-          if (elementsWithClass === undefined) {
-            elementsWithClass = [];
-          }
-          for (var l = 0; l < childElementsWithClass.length; l++) {
-            elementsWithClass.push(childElementsWithClass[l]);
-          }
-        }
-      }
-    }
+  var classNamesLength = classNames.length;
+  if (classNamesLength > 1) {
+    classNames = classNames.sort();
   }
   
+  var searchForClass = function(elementNodeReference) {
+    elementNodeReference = elementNodeReference || document;
+    if (elementNodeReference.classList) {
+      var nodeClasses = Array.prototype.slice.call(
+        elementNodeReference.classList, 0);
+      if (classNamesLength === 1 && nodeClasses.indexOf(classNames[0]) > 0) {
+        elementsWithClass.push(elementNodeReference);
+      } else if (String(classNames) == String(nodeClasses.sort())) {
+        elementsWithClass.push(elementNodeReference);
+      }
+    }
+    if (elementNodeReference.childNodes) {
+      for (var i = 0; i < elementNodeReference.childNodes.length; i++) {
+        searchForClass(elementNodeReference.childNodes[i]);
+      }
+    }
+  };
+  
+  searchForClass();
   return elementsWithClass;
 
 };
