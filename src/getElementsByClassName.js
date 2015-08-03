4e2a01619b9899744923011ca70aed15667be87a
Donald Steinert <steinert.donald@gmail.com>
2015-08-03 12:11:42 -0400

    refactor get elements by class name

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -6,20 +6,16 @@
 // But instead we're going to implement it from scratch:
 var getElementsByClassName = function(className) {
   var elementsWithClass = [];
-  var classNames = className.split(' ');
-  var classNamesLength = classNames.length;
-  if (classNamesLength > 1) {
-    classNames = classNames.sort();
-  }
+  var classNames = className.split(' ').sort();
   
   var searchForClass = function(elementNodeReference) {
     elementNodeReference = elementNodeReference || document;
     if (elementNodeReference.classList) {
       var nodeClasses = Array.prototype.slice.call(
         elementNodeReference.classList, 0);
-      if (classNamesLength === 1 && nodeClasses.indexOf(classNames[0]) > 0) {
+      if (classNames.length === 1 && nodeClasses.indexOf(classNames[0]) > 0) {
         elementsWithClass.push(elementNodeReference);
-      } else if (String(classNames) == String(nodeClasses.sort())) {
+      } else if (String(classNames) === String(nodeClasses.sort())) {
         elementsWithClass.push(elementNodeReference);
       }
     }
@@ -32,5 +28,4 @@ var getElementsByClassName = function(className) {
   
   searchForClass();
   return elementsWithClass;
-
 };
